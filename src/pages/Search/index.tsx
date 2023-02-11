769e3e55195ef83397249cc4d4dac7e43c7f67d6
Guilherme Sandoval <guijpjs@gmail.com>
2023-02-11 15:59:33 -0300

    Create Structure to page Search

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,16 +1,42 @@
 import React, { useEffect } from 'react'
 import { useSearch } from '../../contexts/SearchContext';
+import { useBooks } from '../../services/hooks/useBooks';
+
+import * as S from './styles';
+import { BookCard } from '../../components/BookCard';
 
 function Search() {
-  const queryParams = new URLSearchParams(window.location.search);
   const { searchValue } = useSearch();
+  const { data, isLoading } = useBooks(searchValue);
 
-  console.log('searchValue')
-  console.log(searchValue)
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
 
+  console.log('data')
+  console.log(data)
 
   return (
-    <div>Search</div>
+    <S.Container>
+      <S.FilterAside>
+        <h4>Filtrar</h4>
+        <p>Preço</p>
+        <input type="checkbox" name="" id="" />
+        <input type="checkbox" name="" id="" />
+        <input type="checkbox" name="" id="" />
+        <input type="checkbox" name="" id="" />
+        <p>Disponível para venda</p>
+        <input type="checkbox" name="" id="" />
+        <input type="checkbox" name="" id="" />
+        <p>Formatos disponíveis</p>
+        <input type="checkbox" name="" id="" />
+        <input type="checkbox" name="" id="" />
+      </S.FilterAside>
+      <S.BooksList>
+        <h4>Resultados para "{searchValue}":</h4>
+        <p>Lista aqui</p>
+      </S.BooksList>
+    </S.Container>
   )
 }
 
--- src/services/hooks/useBooks.ts
+++ src/services/hooks/useBooks.ts
@@ -4,10 +4,27 @@ import { IBook } from "../../interfaces/boook";
 
 // https://www.googleapis.com/books/v1/volumes?q=a+subject:adventure&startIndex=0&maxResults=10
 
-async function getBooks(category: string): Promise<IBook[]> {
-  console.log("chegou aqui no getBooks");
-  console.log(category);
+async function getBooks(query: string): Promise<IBook[]> {
+  const { data } = await apiUrl.get("/volumes", {
+    params: {
+      q: query,
+      startIndex: 0,
+      maxResults: 10,
+    },
+  });
 
+  const formattedData = data.items.map((book: any) => {
+    return {
+      id: book.id,
+      title: book.volumeInfo.title,
+      image: book.volumeInfo.imageLinks?.thumbnail,
+      author: book.volumeInfo.authors,
+    };
+  });
+
+  return formattedData;
+}
+async function getBooksCategory(category: string): Promise<IBook[]> {
   const { data } = await apiUrl.get("/volumes", {
     params: {
       q: `a+subject:${category}`,
@@ -29,7 +46,16 @@ async function getBooks(category: string): Promise<IBook[]> {
 }
 
 export function useBooksCategory(category: string) {
-  return useQuery(["books", category], () => getBooks(category), {
+  return useQuery(
+    ["booksCategory", category],
+    () => getBooksCategory(category),
+    {
+      staleTime: Infinity,
+    }
+  );
+}
+export function useBooks(query: string) {
+  return useQuery(["books", query], () => getBooks(query), {
     staleTime: Infinity,
   });
 }
